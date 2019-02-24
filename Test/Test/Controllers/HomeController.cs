4bdd3be4fc24fd7d7eff2917ac628422da2d7385
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-02-24 17:09:43 +0100

    removed some usings and unused files

--- Test/Test/Controllers/HomeController.cs
+++ Test/Test/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Test.Models;
 
 namespace Test.Controllers
@@ -11,11 +7,6 @@ namespace Test.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
