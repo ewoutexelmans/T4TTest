c2793f338c20a04543598d734635a41018c7fb7c
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-02-24 17:18:08 +0100

    added controller method for file

--- Test/Test/Controllers/HomeController.cs
+++ Test/Test/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Test.Models;
+using Test.Services;
 
 namespace Test.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IFileReader _fileReader;
+
+        public HomeController(IFileReader fileReader)
+        {
+            _fileReader = fileReader;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> StreamFile(IFormFile file)
+        {
+            var contents = await _fileReader.Read(file);
+            return Ok(contents);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
