e183ae20af4040383c4618eaa01c9b9169c6c4f7
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-02-26 11:49:08 +0100

    added track model

---
+++ Test/Test/Models/Track.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Test.Models
+{
+    public class Track
+    {
+        public IEnumerable<Talk> MorningTalks { get; set; }
+        public IEnumerable<Talk> AfternoonTalks { get; set; }
+    }
+}
